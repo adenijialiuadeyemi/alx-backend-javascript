0475dac29eef6a9b3db218ac4e7c2da7e4363ff3
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-05-01 04:13:54 +0000

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Adeniji Aliu Adeyemi <https://github.com/adenijialiuadeyemi>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Adeniji Aliu Adeyemi <https://github.com/adenijialiuadeyemi>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
